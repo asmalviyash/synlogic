a5321d58f863165f6ee287ca726d92ca0529fa49
asmalviyash <100632769+asmalviyash@users.noreply.github.com>
2022-02-28 23:32:48 +0000

    Add files via upload

---
+++ SynalogikrestApplication.java
@@ -0,0 +1,13 @@
+package com.synalogikrest.synalogikrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynalogikrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynalogikrestApplication.class, args);
+    }
+
+}
